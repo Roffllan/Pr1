9b278c6caf995adeabda496a08956d605511aaf4
Roffllan <stepan.nikitin2004@gmail.com>
2023-04-01 04:54:39 +0800

    TicTac1

--- src/main/TicTacToe.java
+++ src/main/TicTacToe.java
@@ -1,47 +1,40 @@
 package main;
+
 public class TicTacToe {
     public static class GameXO {
         private final int fieldSize;
-        private final String[][] field;
-        private final String emptyField = "-";
-        private final String player1 = "x";
-        private final String player2 = "o";
+        private final Players[][] field;
+        public enum Players{
+            X , O, emp
+        }
 
         public GameXO(int fieldSize){
             this.fieldSize =fieldSize;
-            this.field = new String[fieldSize][fieldSize];
+            this.field = new Players[fieldSize][fieldSize];
             for (int i = 0; i < fieldSize; i++){
                 for(int j = 0; j < fieldSize; j++){
-                    this.field[i][j] = emptyField;
+                    this.field[i][j] = Players.emp;
                 }
             }
 
         }
-        public void addCharX(int num){
-            if (this.field[pos(num)[0]][pos(num)[1]].contains(emptyField)){
-                this.field[pos(num)[0]][pos(num)[1]] = player1;
+        public void addCharX(int num0, int num1){
+            if (this.field[num0][num1] == Players.emp){
+                this.field[num0][num1] = Players.X;
             }
         }
-        public void addCharO(int num){
-            if (this.field[pos(num)[0]][pos(num)[1]].contains(emptyField)){
-                this.field[pos(num)[0]][pos(num)[1]] = player2;
+        public void addCharO(int num0, int num1){
+            if (this.field[num0][num1] == Players.emp){
+                this.field[num0][num1] = Players.O;
             }
         }
-        public String cleaner(int num){
-            this.field[pos(num)[0]][pos(num)[1]] = emptyField;
-            return this.field[pos(num)[0]][pos(num)[1]];
+        public Players cleaner(int num0, int num1){
+            this.field[num0][num1] = Players.emp;
+            return this.field[num0][num1];
         }
 
-        public String getState(int num){
-            return this.field[pos(num)[0]][pos(num)[1]];
-        }
-        private int[] pos(int num){
-            int row = 0;
-            while (this.fieldSize <= num - 1) {
-                row++;
-                num -= this.fieldSize;
-            }
-            return new int[]{row, num-1};
+        public Players getState(int num0, int num1){
+            return this.field[num0][num1];
         }
         public int maxXDiag(){
             int max_lenght = 0;
@@ -49,11 +42,11 @@ public class TicTacToe {
             int counterD2 = 0;
             int kol = fieldSize;
             for (int count = 0; count < fieldSize; count++) {
-                if (!this.field[count][count].contains(emptyField) && this.field[count][count].contains(player1)){
+                if (this.field[count][count] != Players.emp && this.field[count][count] == Players.X){
                     ++counterD1;
                     max_lenght = Math.max(counterD1, max_lenght);
                 }
-                if (!this.field[--kol][count].contains(emptyField) && this.field[kol][count].contains(player1)){
+                if (this.field[--kol][count] != Players.emp && this.field[kol][count] == Players.X){
                     ++counterD2;
                     max_lenght = Math.max(counterD2, max_lenght);
                 }
@@ -66,11 +59,11 @@ public class TicTacToe {
             int max_lenght = 0;
             int kol = fieldSize;
             for (int count = 0; count < fieldSize; count++) {
-                if (this.field[count][count] != null && this.field[count][count].contains(player2)){
+                if (this.field[count][count] != null && this.field[count][count] == Players.O){
                     ++counterD1;
                     max_lenght = Math.max(counterD1, max_lenght);
                 }
-                if (this.field[--kol][count] != null && this.field[kol][count].contains(player2)){
+                if (this.field[--kol][count] != null && this.field[kol][count] == Players.O){
                     ++counterD2;
                     max_lenght = Math.max(counterD2, max_lenght);
                 }
@@ -83,12 +76,12 @@ public class TicTacToe {
             int counterV = 0;
             for (int row = 0; row < fieldSize; row++) {
                 for (int col = 0; col < fieldSize; col++) {
-                    if (!this.field[row][col].contains(emptyField) && this.field[row][col].contains(player1)){
+                    if (this.field[row][col] != Players.emp && this.field[row][col] == Players.X){
                         counterH++;
                         max_lenght = Math.max(counterH, max_lenght);
                     }
                     else counterH = 0;
-                    if (!this.field[col][row].contains(emptyField) && this.field[col][row].contains(player1)){
+                    if (this.field[col][row] != Players.emp && this.field[col][row] == Players.X){
                         counterV++;
                         max_lenght = Math.max(counterV, max_lenght);
                     }
@@ -103,12 +96,12 @@ public class TicTacToe {
             int counterV = 0;
             for (int row = 0; row < fieldSize; row++) {
                 for (int col = 0; col < fieldSize; col++) {
-                    if (!this.field[row][col].contains(emptyField) && this.field[row][col].contains(player2)){
+                    if (this.field[row][col] != Players.emp && this.field[row][col]== Players.O){
                         counterH++;
                         max_lenght = Math.max(counterH, max_lenght);
                     }
                     else counterH = 0;
-                    if (!this.field[col][row].contains(emptyField) && this.field[col][row].contains(player2)){
+                    if (this.field[col][row] != Players.emp && this.field[col][row] == Players.O){
                         counterV++;
                         max_lenght = Math.max(counterV, max_lenght);
                     }
--- src/test/TicTacToeTest.java
+++ src/test/TicTacToeTest.java
@@ -1,5 +1,4 @@
 package test;
-
 import main.TicTacToe;
 import org.junit.Test;
 
@@ -12,78 +11,78 @@ public class TicTacToeTest {
     @Test
     public void testAddTic() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharX(1);
-        addChar.addCharX(2);
-        addChar.addCharX(3);
-        assertEquals("x", addChar.getState(1));
-        assertEquals("x", addChar.getState(2));
-        assertEquals("x", addChar.getState(3));
-        assertEquals("-", addChar.getState(5));
+        addChar.addCharX(0,0);
+        addChar.addCharX(0,1);
+        addChar.addCharX(0,2);
+        assertEquals(TicTacToe.GameXO.Players.X, addChar.getState(0,0));
+        assertEquals(TicTacToe.GameXO.Players.X, addChar.getState(0,1));
+        assertEquals(TicTacToe.GameXO.Players.X, addChar.getState(0,2));
+        assertEquals(TicTacToe.GameXO.Players.emp, addChar.getState(1,1));
     }
 
 
     @Test
     public void testAddTac() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharO(7);
-        addChar.addCharO(8);
-        addChar.addCharO(9);
-        assertEquals("o", addChar.getState(7));
-        assertEquals("o", addChar.getState(8));
-        assertEquals("o", addChar.getState(9));
-        assertEquals("-", addChar.getState(6));
+        addChar.addCharO(2,0);
+        addChar.addCharO(2,1);
+        addChar.addCharO(2,2);
+        assertEquals(TicTacToe.GameXO.Players.O, addChar.getState(2,0));
+        assertEquals(TicTacToe.GameXO.Players.O, addChar.getState(2,1));
+        assertEquals(TicTacToe.GameXO.Players.O, addChar.getState(2,2));
+        assertEquals(TicTacToe.GameXO.Players.emp, addChar.getState(1,2));
     }
     @Test
     public void testRemove() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharX(7);
-        addChar.addCharO(8);
-        addChar.addCharX(9);
-        assertEquals("-", addChar.cleaner(7));
-        assertEquals("-", addChar.cleaner(8));
-        assertEquals("-", addChar.cleaner(9));
+        addChar.addCharX(2,0);
+        addChar.addCharO(2,1);
+        addChar.addCharX(2,2);
+        assertEquals(TicTacToe.GameXO.Players.emp, addChar.cleaner(2,0));
+        assertEquals(TicTacToe.GameXO.Players.emp, addChar.cleaner(2,1));
+        assertEquals(TicTacToe.GameXO.Players.emp, addChar.cleaner(2,2));
     }
     @Test
     public void longestDiagonalTic() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharX(1);
-        addChar.addCharX(3);
-        addChar.addCharO(2);
-        addChar.addCharO(6);
-        addChar.addCharO(4);
-        addChar.addCharX(5);
+        addChar.addCharX(0,0);
+        addChar.addCharX(0,2);
+        addChar.addCharO(0,1);
+        addChar.addCharO(1,2);
+        addChar.addCharO(1,0);
+        addChar.addCharX(1,1);
         assertEquals(2, addChar.maxXDiag());
     }
     @Test
     public void longestSequenceTic() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharX(1);
-        addChar.addCharX(2);
-        addChar.addCharX(6);
-        addChar.addCharO(8);
-        addChar.addCharO(9);
+        addChar.addCharX(0,0);
+        addChar.addCharX(0,1);
+        addChar.addCharX(1,2);
+        addChar.addCharO(2,1);
+        addChar.addCharO(2,2);
         assertEquals(2, addChar.maxX());
     }
     @Test
     public void longestSequenceTac() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharO(1);
-        addChar.addCharX(2);
-        addChar.addCharX(6);
-        addChar.addCharO(4);
-        addChar.addCharO(7);
-        addChar.addCharO(9);
+        addChar.addCharO(0,0);
+        addChar.addCharX(0,1);
+        addChar.addCharX(1,2);
+        addChar.addCharO(1,0);
+        addChar.addCharO(2,0);
+        addChar.addCharO(2,2);
         assertEquals(3, addChar.maxO());
     }
     @Test
     public void longestDiagonalTac() throws IOException {
         TicTacToe.GameXO addChar = new TicTacToe.GameXO(3);
-        addChar.addCharX(1);
-        addChar.addCharX(4);
-        addChar.addCharO(7);
-        addChar.addCharO(5);
-        addChar.addCharO(3);
-        addChar.addCharX(8);
+        addChar.addCharX(0,0);
+        addChar.addCharX(1,0);
+        addChar.addCharO(2,0);
+        addChar.addCharO(1,1);
+        addChar.addCharO(0,2);
+        addChar.addCharX(2,1);
         assertEquals(3, addChar.maxODiag());
     }
 }
